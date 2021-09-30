9dbd43adfb4afb61ac605f0589e9ff833691c9ef
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-09-30 15:47:44 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/vinim/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.vinim.workshopmongo.config;
+
+import com.vinim.workshopmongo.domain.User;
+import com.vinim.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
