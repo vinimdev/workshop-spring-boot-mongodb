d553516de43b7b4ebdde7c3617307bbcf95235db
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-09-30 14:58:36 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/vinim/workshopmongo/domain/User.java
+++ src/main/java/com/vinim/workshopmongo/domain/User.java
@@ -1,9 +1,15 @@
 package com.vinim.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document("user")
 public class User implements Serializable {
+
+    @Id
     private String id;
     private String name;
     private String email;
