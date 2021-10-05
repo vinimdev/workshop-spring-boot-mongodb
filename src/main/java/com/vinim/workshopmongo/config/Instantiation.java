d3e0428e43641504158a3884e3db5cb59eaec4a1
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-10-04 22:09:12 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/vinim/workshopmongo/config/Instantiation.java
+++ src/main/java/com/vinim/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.vinim.workshopmongo.config;
 
 import com.vinim.workshopmongo.domain.Post;
 import com.vinim.workshopmongo.domain.User;
+import com.vinim.workshopmongo.dto.AuthorDTO;
 import com.vinim.workshopmongo.repository.PostRepository;
 import com.vinim.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,22 +34,24 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
         Post post1 = new Post(
                                 null,
                                 sdf.parse("21/03/2018"),
                                 "Partiu viagem",
                                 "Vou viajar para São Paulo. Abraços!",
-                                maria
+                                new AuthorDTO(maria)
                             );
         Post post2 = new Post(
                                 null,
                                 sdf.parse("23/03/2018"),
                                 "Bom dia",
                                 "Acordei feliz hoje!",
-                                maria
+                                new AuthorDTO(maria)
                             );
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
