1e1b090a14e00d68f7f35efea2741745807b0c60
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-10-04 23:48:11 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/vinim/workshopmongo/config/Instantiation.java
+++ src/main/java/com/vinim/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.vinim.workshopmongo.config;
 import com.vinim.workshopmongo.domain.Post;
 import com.vinim.workshopmongo.domain.User;
 import com.vinim.workshopmongo.dto.AuthorDTO;
+import com.vinim.workshopmongo.dto.CommentDTO;
 import com.vinim.workshopmongo.repository.PostRepository;
 import com.vinim.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.TimeZone;
 
 @Configuration
@@ -51,6 +53,24 @@ public class Instantiation implements CommandLineRunner {
                                 new AuthorDTO(maria)
                             );
 
+        CommentDTO c1 = new CommentDTO(
+                                        "Boa viagem mano",
+                                        sdf.parse("21/03/2018"),
+                                        new AuthorDTO(alex)
+                                      );
+        CommentDTO c2 = new CommentDTO(
+                                        "Aproveite",
+                                            sdf.parse("22/03/2018"),
+                                            new AuthorDTO(bob)
+                                        );
+        CommentDTO c3 = new CommentDTO(
+                                        "Tenha um ótimo dia!",
+                                        sdf.parse("23/03/2018"),
+                                        new AuthorDTO(alex)
+                                        );
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Collections.singletonList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
