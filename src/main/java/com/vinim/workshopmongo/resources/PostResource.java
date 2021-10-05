1571fdc27bebaca4bdf4991d1b1ee57d2eb386dd
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-10-05 15:03:17 -0300

    Consulta simples com query methods

--- src/main/java/com/vinim/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vinim/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.vinim.workshopmongo.resources;
 
 import com.vinim.workshopmongo.domain.Post;
+import com.vinim.workshopmongo.resources.util.URL;
 import com.vinim.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,5 +22,14 @@ public class PostResource {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 }
